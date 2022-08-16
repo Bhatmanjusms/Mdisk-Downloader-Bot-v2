6ee19697ead996179dc1b1d9b9a7d8a4cb76efe2
kevinnadar <jesikamaraj@gmail.com>
2022-08-16 12:39:55 +0530

    init

--- database/__init__.py
+++ database/__init__.py
@@ -3,7 +3,7 @@ from config import MONGODB, DATABASE_NAME, COLLECTION_NAME
 import ssl
 
 URI = MONGODB
-db = pymongo.MongoClient(URI, ssl_cert_reqs=ssl.CERT_NONE)
+db = pymongo.MongoClient(URI)
 client = db[DATABASE_NAME]
 collection = client[COLLECTION_NAME]
 
